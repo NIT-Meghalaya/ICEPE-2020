c5de36d84b9e2164576dc5f6890ed67660cb4b85
Abhushan <abhushanadhikarijoshi@gmail.com>
2020-02-24 11:40:04 +0530

    Added more backgrounds on home page as requested

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const backgroundsLength = 3;
+  const backgroundsLength = 6;
   const slider = document.querySelector(".hero-box");
   let i = 1;
   setInterval(() => {
